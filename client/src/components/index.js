a3e0b2da90a26a4ad4eaa60c1e8f9af9911196b3
Adeyemi <110929615+Adeyemi16@users.noreply.github.com>
2023-09-21 11:59:54 +0100

    fixed updateprofile

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -9,8 +9,8 @@ import Footer from "./Footer";
 import ProductDetail from "./ProductDetail";
 import ImageModal from "./ImageModal";
 import Paypal from "./Paypal";
-import UpdateProfile from "./Updateprofile";
+import UpdateProfile from "./UpdateProfile";
 import Error from "./Error";
 
 
-export { Root, FeaturedProduct, Condition, ProductCard, PageLayout, Preorder, Headings , Footer, ProductDetail, ImageModal, Paypal, UpdateProfile, Error }
+export { Root, FeaturedProduct, Condition, ProductCard, PageLayout, Preorder, Headings, Footer, ProductDetail, ImageModal, Paypal, UpdateProfile, Error }
